42f0351b7da9781cbbc73aed91302fe1440870de
Dheeraj <dheeraj.techbuddiesit@gmail.com>
2025-03-06 15:29:37 +0530

    fix issue

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+// const port = 3000
 
 let records = [];
 
@@ -39,6 +40,8 @@ router.get('/demo', (req, res) => {
     },
   ]);
 });
-
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
