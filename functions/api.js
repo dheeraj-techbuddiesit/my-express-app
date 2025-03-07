70052cf5874427517781c4b0cdeaafca7cdbb929
dheeraj-techbuddiesit <dheeraj.techbuddiesit@gmail.com>
2025-03-07 15:44:11 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,8 +3,6 @@ const serverless = require("serverless-http");
 const errorHandler = require('./middleware/errorHandler');
 const app =express();
 const connectDB = require('./confiq/dbConnection');
-const dotenv = require('dotenv').config();
-const port = "5000";
 const router = express.Router();
 app.use(express.json());
 connectDB();
@@ -12,8 +10,5 @@ app.use('/api/contact', require('./routes/contactRoutes'));
 app.use('/api/user', require('./routes/userRouter'));
 app.use('/test',function(req,res){res.send('Server code working fine')});
 app.use(errorHandler);
-app.listen(port, ()=>{
-    console.log("Server is running on port", port );
-});
 app.use('/functions', router);
 module.exports.handler = serverless(app);
